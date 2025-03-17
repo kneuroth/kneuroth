1074974ad22166d27c86940e2e4b16b786718936
Kelly N <kelly.neuroth@outlook.com>
2025-03-17 17:45:01 -0400

    Check out banner Michael!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, HeaderComponent],
+  imports: [RouterOutlet, ButtonModule, HeaderComponent, BannerComponent],
   templateUrl: './app.component.html',
   standalone: true,
 })
